401ebf02467b6dcba052cbd801b59c4390e3cf6c
teruya <c22015@std.it-college.ac.jp>
2023-03-22 18:44:44 +0900

    CSVの読み込みテスト

---
+++ Assets/Scripts/Nosuke/ReadCSV_nos.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class ReadCSV_nos : MonoBehaviour
+{
+    TextAsset csvFile; // CSVファイル
+    List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+    void Start()
+    {
+        csvFile = Resources.Load("CSVs/test") as TextAsset; // Resouces下のCSV読み込み
+        StringReader reader = new StringReader(csvFile.text);
+        // , で分割しつつ一行ずつ読み込み
+        // リストに追加していく
+        while (reader.Peek() != -1) // reader.Peaekが-1になるまで
+        {
+            string line = reader.ReadLine(); // 一行ずつ読み込み
+            csvDatas.Add(line.Split(',')); // , 区切りでリストに追加
+        }
+        // csvDatas[行][列]を指定して値を自由に取り出せる
+        Debug.Log(int.Parse(csvDatas[1][1]));
+        Debug.Log(float.Parse(csvDatas[1][1]));
+    }
+}
